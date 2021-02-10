4d9f0ab7b9952bc572b87cc51bc34c3506b35ed6
clarissalimoa <clarissalimoa12@gmail.com>
2021-02-10 15:06:49 +0700

    init

---
+++ app/src/main/java/id/ac/ubaya/informatika/stefmoa_week1/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ubaya.informatika.stefmoa_week1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
